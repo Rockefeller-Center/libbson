955c7cffc9723d1be02d55d303323124f89f9518
Ezra Celli <ezra@bighuman.com>
2022-10-24 07:25:00 -0400

    :green_heart: explicitly disable CXX support
    fixes netlify ci build

--- libbson-sys/build.rs
+++ libbson-sys/build.rs
@@ -1,5 +1,6 @@
 fn main() {
     let dst = cmake::Config::new("mongo-c-driver")
+        .configure_arg("-DCMAKE_CXX_COMPILER=")
         .configure_arg("-DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF")
         .configure_arg("-DENABLE_EXAMPLES=OFF")
         .configure_arg("-DENABLE_MONGOC=OFF")
